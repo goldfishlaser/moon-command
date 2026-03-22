df1ff446549766a92f0c4d95f43a1ed362cfb138
Melanie <goldfishlaser@protonmail.com>
2026-03-21 22:21:49 -0700

    Fix to actually get integer

--- src/main.c
+++ src/main.c
@@ -32,7 +32,7 @@ const char* getMoonPhase(int year, int month, int day) {
     e = 30.6 * month; 
     jd = c + e + day - 694039.09; // jd is total days elapsed 
     jd /= 29.5305882; // divide by the moon cycle (29.53 days) 
-    b = jd; // int(jd) -> b, take integer part of jd 
+    b = Math.floor(jd); // int(jd) -> b, take integer part of jd 
     jd -= b; // subtract integer part to leave fractional part of original jd 
     b = jd * 8 + 0.5; // scale fraction from 0-8 and round by adding 0.5 
     b = b & 7; // 0 and 8 are the same so turn 8 into 0 
