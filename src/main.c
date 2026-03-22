18b4308e34978eae0f1bf8fa1371028707d05f62
Melanie <goldfishlaser@protonmail.com>
2026-03-21 23:04:45 -0700

    oops

--- src/main.c
+++ src/main.c
@@ -28,13 +28,13 @@ const char* getMoonPhase(int year, int month, int day) {
     
     if (month < 3) { year--; month += 12; } ++month; 
     
-    c = Math.floor(365.25 * year);
-    e = Math.floor(30.6 * month);
+    c = (int)floor(365.25 * year);
+    e = (int)floor(30.6 * month);
     jd = c + e + day - 694039.09; // jd is total days elapsed 
     jd /= 29.5305882; // divide by the moon cycle (29.53 days) 
-    b = Math.floor(jd); // int(jd) -> b, take integer part of jd 
+    b = (int)floor(jd);// int(jd) -> b, take integer part of jd 
     jd -= b; // subtract integer part to leave fractional part of original jd 
-    b = jd * 8 + 0.5; // scale fraction from 0-8 and round by adding 0.5 
+    b = (int)floor(jd * 8 + 0.5); // scale fraction from 0-8 and round by adding 0.5 
     b = b & 7; // 0 and 8 are the same so turn 8 into 0 
     
     switch (b) { 
